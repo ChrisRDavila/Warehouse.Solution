d3efa8b0700201074d332ac607499883bf320eb9
ChrisRDavila <crossdavila@gmail.com>
2023-07-07 14:17:08 -0700

    updating models WIP

---
+++ WarehouseProject/Models/Warehouse.cs
@@ -0,0 +1,10 @@
+namespace WarehouseProject.Models.Models
+{
+  public class Warehouse
+  {
+    public int WarehouseId { get; set; }
+    public string WarehouseName { get; set; }
+    public string Location { get; set; }
+    public string WHDescription { get; set; }
+  }
+}
