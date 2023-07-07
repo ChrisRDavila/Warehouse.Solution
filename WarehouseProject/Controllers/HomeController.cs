d7d5c4daca2e7f06b48ce58b5f775d9abe5d7a01
ChrisRDavila <crossdavila@gmail.com>
2023-07-07 16:05:56 -0700

    add applicationUser model to set up for log in, and home controller

--- WarehouseProject/Controllers/HomeController.cs
+++ WarehouseProject/Controllers/HomeController.cs
@@ -1,13 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using WarehouseProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
-
-
-namespace ProjectName.Controllers
+namespace WarehouseProject.Controllers
 {
-  public class HomeController : Controller
-  {
-    [Route("/")]
-    // Notice the changes below!
-    public ActionResult File() { return View(); }
-  }
+    public class HomeController : Controller
+    {
+      private readonly WarehouseProjectContext _db;
+      private readonly UserManager<ApplicationUser> _userManager;
+
+      public HomeController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+      {
+        _userManager = userManager;
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public async Task<ActionResult> Index()
+      {
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        if (currentUser != null)
+        {
+          Product[] products = _db.Products
+                      .Where(entry => entry.User.Id == currentUser.Id)
+                      .ToArray();
+          model.Add("products", products);
+          Warehouse[] warehouses = _db.Warehouses
+                      .Where(entry => entry.User.Id == currentUser.Id)
+                      .ToArray();
+          model.Add("warehouses", warehouses);
+        }
+        return View(model);
+      }
+    }
 }
