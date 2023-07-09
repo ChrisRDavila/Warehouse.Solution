1435e4c66f2fadb3218f9ad54a9b66a1b8eddd1b
ChrisRDavila <crossdavila@gmail.com>
2023-07-08 19:09:37 -0700

    ADD warehouse and create product added

--- WarehouseProject/Models/Warehouse.cs
+++ WarehouseProject/Models/Warehouse.cs
@@ -16,6 +16,8 @@ namespace WarehouseProject.Models
     [Required(ErrorMessage = "Please enter a state for the warehouse.")]
     public string State { get; set; }
     [Required(ErrorMessage = "Please enter a zipcode for the warehouse.")]
+    public string Zipcode { get; set; }
+    [Required(ErrorMessage = "Please enter a phone number for the warehouse desciption.")]
     public string WHDescription { get; set; }
     public List<WarehouseProduct> JoinEntities { get; set; }
   }
