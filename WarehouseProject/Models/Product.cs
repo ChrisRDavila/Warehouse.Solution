27fb87ebebe4741530cdbb882112802f4a5342b5
ChrisRDavila <crossdavila@gmail.com>
2023-07-25 11:23:44 -0700

    WIP. Mapping out join table views for application

--- WarehouseProject/Models/Product.cs
+++ WarehouseProject/Models/Product.cs
@@ -11,13 +11,13 @@ namespace WarehouseProject.Models
     public string ProductTypeId { get; set; } 
     [Required(ErrorMessage = "Please enter a type for the product.")]
     public ProductType ProductType { get; set; }
-    public string Description { get; set; }
     [Required(ErrorMessage = "Please enter a barcode for the product.")]
     public string Barcode { get; set; }
+    public string Description { get; set; }
     public DateTime DateReceived { get; set; }
     public ApplicationUser User { get; set; }
     public Boolean Picked { get; set; }
     public string Bin { get; set; }
-    ////
+    
   }
 }
